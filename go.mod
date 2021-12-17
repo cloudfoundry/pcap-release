b297175c4e60aa0d050b281c405e181613608378
Dominik Froehlich <dominik.froehlich@sap.com>
2021-12-17 16:13:20 +0100

    Read from instance id working (wip)

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/domdom82/pcap-server
 
 go 1.16
 
-require github.com/google/gopacket v1.1.19
+require (
+	github.com/containerd/go-runc v1.0.0 // indirect
+	github.com/google/gopacket v1.1.19
+	github.com/vishvananda/netns v0.0.0-20211101163701-50045581ed74
+)
--- main.go
+++ main.go
@@ -1,27 +1,114 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
+	"errors"
 	"fmt"
+	"github.com/containerd/go-runc"
 	"github.com/google/gopacket"
 	"github.com/google/gopacket/layers"
 	"github.com/google/gopacket/pcap"
 	"github.com/google/gopacket/pcapgo"
+	"github.com/vishvananda/netns"
+	"io/ioutil"
+	"net"
 	"os"
+	"runtime"
 )
 
 func main() {
 
-	usage := "usage: pcap-server <device> <filter> <file.pcap>"
+	usage := "usage: pcap-server <instance-id> <device> <filter> <file.pcap>"
 
 	if len(os.Args) < 4 {
 		fmt.Println(usage)
 		os.Exit(1)
 	}
 
-	device := os.Args[1]
-	filter := os.Args[2]
-	outfile := os.Args[3]
+	instanceId := os.Args[1]
+	device := os.Args[2]
+	filter := os.Args[3]
+	outfile := os.Args[4]
 
+	// CF-SPECIFIC PARTS BEGIN
+	type ContainerConfig struct {
+		Handle   string `json:"handle"`
+		Ip       string `json:"ip"`
+		Metadata struct {
+			AppId             string `json:"app_id"`
+			SpaceId           string `json:"space_id,omitempty"`
+			OrgId             string `json:"org_id,omitempty"`
+			Ports             string `json:"ports,omitempty"`
+			ContainerWorkload string `json:"container_workload,omitempty"`
+			PolicyGroupId     string `json:"policy_group_id,omitempty"`
+		} `json:"metadata"`
+	}
+	type ContainerKeys struct {
+		Keys map[string]*ContainerConfig
+	}
+
+	// Load container store
+	var store ContainerKeys
+	storeFile := "/var/vcap/data/container-metadata/store.json"
+	storeData, err := ioutil.ReadFile(storeFile)
+	if err != nil {
+		panic(err)
+	}
+
+	err = json.Unmarshal(storeData, &store.Keys)
+	if err != nil {
+		panic(err)
+	}
+
+	// Get runc container id from instance id
+	var containerId string
+	for key, value := range store.Keys {
+		if value.Metadata.AppId == instanceId {
+			containerId = key
+		}
+	}
+
+	if containerId == "" {
+		panic(errors.New("Could not find container for instance id " + instanceId))
+	}
+
+	// Get pid from runc container state
+	ctx := context.Background()
+	runcClient := &runc.Runc{
+		Command: "/var/vcap/packages/runc/bin/runc",
+		Root:    "/run/containerd/runc/garden",
+	}
+
+	container, err := runcClient.State(ctx, containerId)
+	if err != nil {
+		panic(err)
+	}
+
+	pid := container.Pid
+
+	// CF-SPECIFIC PARTS END
+
+	// Lock the OS Thread so we don't accidentally switch namespaces
+	runtime.LockOSThread()
+	defer runtime.UnlockOSThread()
+
+	// Save the current network namespace
+	origns, _ := netns.Get()
+	defer origns.Close()
+
+	// Get namespace from pid
+	newns, _ := netns.GetFromPid(pid)
+	defer newns.Close()
+
+	// Activate network namespace
+	netns.Set(newns)
+
+	// Print interfaces in the namespace
+	ifaces, _ := net.Interfaces()
+	fmt.Printf("Interfaces: %v\n", ifaces)
+
+	// Start capturing packets
 	f, err := os.Create(outfile)
 	if err != nil {
 		panic(err)
@@ -34,7 +121,8 @@ func main() {
 		panic(err)
 	}
 
-	if handle, err := pcap.OpenLive(device, 1600, true, pcap.BlockForever); err != nil {
+	fmt.Println("Listening on " + device + " ...")
+	if handle, err := pcap.OpenLive(device, 65535, true, pcap.BlockForever); err != nil {
 		panic(err)
 	} else if err := handle.SetBPFFilter(filter); err != nil { // optional
 		panic(err)
@@ -49,4 +137,6 @@ func main() {
 		}
 	}
 
+	// Switch back to the original namespace
+	netns.Set(origns)
 }
