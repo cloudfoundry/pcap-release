fb8242769c0b5d10cb543aa9e823eeb6b4f02700
Rizwan <m.rizwan.shaik@sap.com>
2022-10-07 12:37:39 +0200

    Capturing tests for bosh api server

--- src/pcap-api/bosh_test.go
+++ src/pcap-api/bosh_test.go
@@ -2,8 +2,9 @@ package main
 
 import (
 	"github.com/cloudfoundry/pcap-release/pcap-api/test"
-	"time"
 	"net/http"
+	"net/url"
+	"time"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
@@ -35,11 +36,6 @@ var _ = Describe("Bosh api basic Tests", func() {
 		AfterEach(func() {
 			pcapApi.Stop()
 		})
-
-		It("can be created", func() {
-			_, err := NewApi(&cfg)
-			Expect(err).To(BeNil())
-		})
 		It("can be started", func() {
 			r, err := http.Get("http://localhost:8080/health")
 			Expect(err).To(BeNil())
@@ -51,6 +47,62 @@ var _ = Describe("Bosh api basic Tests", func() {
 			pcapApi.Stop()
 			_, err = http.Get("http://localhost:8080/health")
 			Expect(err).To(Not(BeNil()))
+		})
+	})
+})
+
+var _ = Describe("Single Instances capture validation errors", func() {
+
+	Context("When the bosh Pcap API is started with the default config", func() {
+		boshAPI := test.MockBoshDirectorAPI(nil)
+		var pcapApi *Api
+		var err error
+
+		cfg := DefaultConfig
+		cfg.BoshDirectorAPI = boshAPI.URL
+		cfg.CfAPI = ""
+		BeforeEach(func() {
+			pcapApi, err = NewApi(&cfg)
+			Expect(err).To(BeNil())
+			go pcapApi.Run()
+			time.Sleep(100 * time.Millisecond)
+		})
+		AfterEach(func() {
+			pcapApi.Stop()
+		})
+
+		It("requires deployment parameter to start capturing", func() {
+			r, err := http.Get("http://localhost:8080/capture/bosh")
+			Expect(err).To(BeNil())
+			Expect(r.StatusCode).To(Equal(400))
+		})
+		It("requires group parameter to start capturing", func() {
+
+			r, err := http.Get("http://localhost:8080/capture/bosh?deployment=haproxy")
+			Expect(err).To(BeNil())
+			Expect(r.StatusCode).To(Equal(400))
+		})
+
+		It("requires authorization header to contain the auth token", func() {
+
+			r, err := http.Get("http://localhost:8080/capture/bosh?deployment=haproxy&group=ha_proxy_z1")
+			Expect(err).To(BeNil())
+			Expect(r.StatusCode).To(Equal(401))
+		})
+
+		It("requires authorization header to contain a valid token", func() {
+			client := http.DefaultClient
+			url, _ := url.Parse("http://localhost:8080/capture/bosh?deployment=haproxy&group=ha_proxy_z1")
+			req := &http.Request{
+				URL: url,
+				Header: map[string][]string{
+					"Authorization": {"not a token"},
+				},
+			}
+
+			r, err := client.Do(req)
+			Expect(err).To(BeNil())
+			Expect(r.StatusCode).To(Equal(403))
 		})
 	})
 })
