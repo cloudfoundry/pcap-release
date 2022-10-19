578164810fb6396ef92462ef844c9b46b3247ec1
M Rizwan Shaik <m.rizwan.shaik@sap.com>
2022-10-19 18:50:38 +0200

    Create mock for Jwt token, adapt tests to use actual generated token

--- src/pcap-api/test/mocks.go
+++ src/pcap-api/test/mocks.go
@@ -1,52 +1,61 @@
 package test
 
 import (
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/pem"
+	"fmt"
 	"io"
 	"net/http"
 	"net/http/httptest"
 	"net/url"
 	"os"
 	"text/template"
+	"time"
+
+	"github.com/golang-jwt/jwt/v4"
 
 	log "github.com/sirupsen/logrus"
 )
 
-func MockjwtAPI(responses map[string]string) *httptest.Server{
+func MockjwtAPI() (*httptest.Server, string) {
 
 	type JwtAPIMock struct {
-
 		UAAUrl string
-
 	}
 
 	var JwtApi JwtAPIMock
 
-
 	mux := http.NewServeMux()
+	ts := httptest.NewServer(mux)
+	jku := ts.URL + "/token_keys"
 
-	mux.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
+	publicpemkey, token := verifyJWTTokenMock(jku)
+
+	json := fmt.Sprintf(`{
+		"kty":   "RSA",
+		"e":     "AQAB",
+		"use":   "sig",
+		"kid":   "uaa-jwt-key-1",
+		"alg":   "RS256",
+		"value": "%s",
+		"n":     "",
+
+	}`, publicpemkey)
+
+	mux.HandleFunc("/token_keys", func(writer http.ResponseWriter, request *http.Request) {
 		writer.Header().Set("Content-Type", "application/json")
 
-		response, ok := responses[request.URL.Path]
-		if !ok {
-			writer.WriteHeader(http.StatusNotFound)
-
-			return
-		}
-
-		writer.Write([]byte(response))
+		writer.Write([]byte(json))
 	})
-
-	ts := httptest.NewServer(mux)
 
 	JwtApi.UAAUrl = ts.URL
 
-
-
-	return ts
+	return ts, token
 }
 
-func MockBoshDirectorAPI(responses map[string]string) *httptest.Server {
+func MockBoshDirectorAPI(responses map[string]string, url string) *httptest.Server {
 	json := `{
 		"name": "bosh-azure-cfn01",
 		"uuid": "f0ceb485-e188-4b9f-b3d5-fb2067aad3c2",
@@ -96,9 +105,71 @@ func MockBoshDirectorAPI(responses map[string]string) *httptest.Server {
 	})
 
 	ts := httptest.NewServer(mux)
-	boshapi.URL = ts.URL
+	boshapi.URL = url
 
 	return ts
+}
+
+func verifyJWTTokenMock(jku string) (string, string) {
+
+	type payload struct {
+		Scope      []string  `json:"scope"`
+		ClientId   string    `json:"client_id"`
+		Cid        string    `json:"cid"`
+		Azp        string    `json:"azp"`
+		Grant_Type string    `json:"grant_type"`
+		UserId     string    `json:"user_id"`
+		Origin     string    `json:"origin"`
+		User       string    `json:"user_name"`
+		Email      string    `json:"email"`
+		Auth_time  time.Time `json:"auth_time"`
+		Rev_sig    string    `json:"rev_sig"`
+		Zid        string    `json:"zid"`
+		jwt.RegisteredClaims
+	}
+
+	// Create the claims
+	claims := payload{
+
+		Scope: []string{"openid",
+			"bosh.admin"},
+		ClientId:   "bosh_cli",
+		Cid:        "bosh_cli",
+		Azp:        "bosh_cli",
+		Grant_Type: "password",
+		UserId:     "f62c94ae-2552-411b-9f8e-9ad181c50b40",
+		Origin:     "uaa",
+		User:       "h.example",
+		Email:      "h.example@192.168.1.11:8443",
+		Auth_time:  time.Now(),
+		Rev_sig:    "14832ae7",
+		Zid:        "uaa",
+
+		RegisteredClaims: jwt.RegisteredClaims{
+			// A usual scenario is to set the expiration time relative to the current time
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(5 * time.Minute)),
+			IssuedAt:  jwt.NewNumericDate(time.Now()),
+			NotBefore: jwt.NewNumericDate(time.Now()),
+			Issuer:    "https://192.168.1.11:8443/oauth/token",
+			Subject:   "f62c94ae-2552-411b-9f8e-9ad181c50b40",
+			Audience:  []string{"openid", "bosh_cli", "bosh"},
+		},
+	}
+	privateKey, err := rsa.GenerateKey(rand.Reader, 2048)
+	publicKey := privateKey.PublicKey
+	publickeybytes := x509.MarshalPKCS1PublicKey(&publicKey)
+	block := &pem.Block{
+		Type:  "PUBLIC KEY",
+		Bytes: publickeybytes,
+	}
+	publicPem := pem.EncodeToMemory(block)
+	token := jwt.NewWithClaims(jwt.SigningMethodRS256, claims)
+	token.Header["jku"] = jku
+	token.Header["kid"] = "uaa-jwt-key-1"
+	ss, err := token.SignedString(privateKey)
+	fmt.Printf("%v %v", ss, err)
+
+	return string(publicPem), ss
 }
 
 func MockCfAPI(responses map[string]string) *httptest.Server {
