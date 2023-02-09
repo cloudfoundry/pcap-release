70a3888c6a6ff3fa293f43f05e42b205a134e47f
Alexander Lais <alexander.lais@sap.com>
2023-02-09 16:16:23 +0100

    Output received messages if test condition failed

--- src/pcap/integration_tests/integration_test.go
+++ src/pcap/integration_tests/integration_test.go
@@ -123,7 +123,7 @@ var _ = Describe("IntegrationTests", func() {
 				capture, messages, err := recvCapture(10, stream)
 				Expect(err).NotTo(HaveOccurred(), "Receiving the first 10 messages")
 				Expect(capture).NotTo(BeNil())
-				Expect(messages).To(HaveLen(10))
+				Expect(messages).To(HaveLen(10), func() string { return fmt.Sprintf("Messages: %+v", messages) })
 				err = stream.Send(stop)
 				Expect(err).NotTo(HaveOccurred(), "Sending stop message")
 
