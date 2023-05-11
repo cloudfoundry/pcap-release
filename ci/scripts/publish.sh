ce9919a3f849fe895156d2ee98e6f611b2096376
Dominik Froehlich <dominik.froehlich@sap.com>
2023-05-11 17:16:29 +0200

    ci: add semantic-release to Dockerfile

--- ci/scripts/publish.sh
+++ ci/scripts/publish.sh
@@ -2,7 +2,7 @@
 
 set -ex
 
-RELEASE_GIT_TAG="$1"
+RELEASE_GIT_TAG="${1:?Release Git Tag is mandatory}"
 mkdir -p "${CONCOURSE_ROOT}/${RELEASE_ROOT}/artifacts"
 
 cp /tmp/pcap-"${RELEASE_GIT_TAG}".tgz "${CONCOURSE_ROOT}/${RELEASE_ROOT}/artifacts/"
