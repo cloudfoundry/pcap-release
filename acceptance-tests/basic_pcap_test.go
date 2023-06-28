b85c45566ebf7f6d4b8642048103a5dcbba47ee5
Dominik Froehlich <dominik.froehlich@sap.com>
2023-06-28 17:49:03 +0200

    fix(pcap-bosh-cli): remove unused type option. only valid for cf.

--- acceptance-tests/basic_pcap_test.go
+++ acceptance-tests/basic_pcap_test.go
@@ -1,8 +1,10 @@
 package acceptance_tests
 
 import (
+	"fmt"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	"github.com/onsi/gomega/gexec"
 	"os"
 	"os/exec"
 	"time"
@@ -31,13 +33,35 @@ var _ = Describe("Pcap Deployment", func() {
 		err = downloadFile(info, "/var/vcap/packages/pcap-api/bin/cli/build/pcap-bosh-cli-linux-amd64", pcapBoshCli, 0755)
 		Expect(err).NotTo(HaveOccurred())
 
-		time.Sleep(2 * time.Hour)
+		By("Starting capture of traffic on pcap-agent instance")
+		pcapFile, err := os.CreateTemp("", "test-*.pcap")
+		Expect(err).NotTo(HaveOccurred())
+		cmdPcap := exec.Command(
+			pcapBoshCli,
+			fmt.Sprintf("-d %s", deploymentNameForTestNode()),
+			"-g pcap-agent",
+			fmt.Sprintf("-o %s", pcapFile.Name()),
+			fmt.Sprintf("-u http://%s:8080/", info.PcapAPIPublicIP),
+			"-v",
+			"-F")
 
-		cmd := exec.Command(pcapBoshCli, "--help")
-
-		helpTest, err := cmd.Output()
+		sessionPcap, err := gexec.Start(cmdPcap, GinkgoWriter, GinkgoWriter)
 		Expect(err).NotTo(HaveOccurred())
 
-		writeLog(string(helpTest))
+		By("Starting ping against pcap-agent instance to produce some traffic")
+		cmdPing := exec.Command("ping", "-c 10", fmt.Sprintf("%s", info.PcapAgentPublicIP))
+		sessionPing, err := gexec.Start(cmdPing, GinkgoWriter, GinkgoWriter)
+		Expect(err).NotTo(HaveOccurred())
+
+		By("Stopping capture after ping has finished")
+		Eventually(sessionPing, time.Minute, time.Second).Should(gexec.Exit(0))
+		sessionPcap.Interrupt()
+		Eventually(sessionPcap, time.Minute, time.Second).Should(gexec.Exit(0))
+
+		By("Checking that the capture has produced a valid pcap file")
+		_ = pcapFile.Close()
+		pcapFileStat, err := pcapFile.Stat()
+		Expect(err).NotTo(HaveOccurred())
+		Expect(pcapFileStat.Size()).To(BeNumerically(">", 0))
 	})
 })
