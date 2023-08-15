22d181cbeefa97375b66a16cbe62f7d08eff6c68
Tamara Boehm <tamara.boehm@sap.com>
2023-08-15 15:30:39 +0200

    fix: bosh CLI not stopping on CTRL+C
    Co-Authored-By: Maximilian Moehl <maximilian.moehl@sap.com>

--- src/pcap/cmd/pcap-bosh-cli/main.go
+++ src/pcap/cmd/pcap-bosh-cli/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	"context"
 	"crypto/x509"
 	"encoding/base64"
 	"encoding/json"
@@ -94,6 +95,9 @@ func main() {
 		}
 	}()
 
+	ctx, cancel := context.WithCancelCause(context.Background())
+	defer cancel(nil)
+
 	// Parse command-line arguments
 	_, err = flags.Parse(&opts)
 	if err != nil {
@@ -138,7 +142,7 @@ func main() {
 	endpointRequest := createEndpointRequest(environment.AccessToken, opts.Deployment, opts.InstanceGroups)
 	captureOptions := createCaptureOptions(opts.Interface, opts.Filter, uint32(opts.SnapLength))
 
-	err = client.CaptureRequest(endpointRequest, captureOptions)
+	err = client.CaptureRequest(ctx, cancel, endpointRequest, captureOptions)
 	if err != nil {
 		return
 	}
